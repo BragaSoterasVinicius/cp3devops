8b6b9ce898beacbccee19a9ae766b427243542e2
BragaSoterasVinicius <viniciussoteras@gmail.com>
2026-05-19 22:01:32 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/_tdspo/cp3devopsrm566230/Cp3devopsrm566230Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspo.cp3devopsrm566230;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp3devopsrm566230Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp3devopsrm566230Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/_tdspo/cp3devopsrm566230/Cp3devopsrm566230ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap._tdspo.cp3devopsrm566230;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp3devopsrm566230ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
